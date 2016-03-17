017d6d31e200dd69f5680af8dbefe88bc880c1ab
Artem Trubitsyn <artem_trubitsyn@epam.com>
2016-03-17 13:57:50 +0200

    Add dotenv

--- gulpfile.js
+++ gulpfile.js
@@ -15,9 +15,7 @@ gulp.task("webpack", function(callback) {
         if(err) {
             throw new gutil.PluginError("webpack", err);
         }
-        gutil.log("[webpack]", stats.toString({
-            // output options
-        }));
+        gutil.log("[webpack]", stats.toString());
         callback();
     });
 });
